2eebcb1d78b1bd0939bba566c25b1e5be3df1567
smit maniya <22it072@charusat.edu.in>
2025-03-29 10:43:43 +0530

    update something

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,9 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:indic_translator_pro/app/app.dart';
 
-import 'package:lab1/main.dart';
+import 'package:indic_translator_pro/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
